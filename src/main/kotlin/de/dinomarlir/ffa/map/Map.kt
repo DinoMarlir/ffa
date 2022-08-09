65be9919b8f2257e4fc1d34d4b412dd3321a61b4
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2022-08-09 14:51:31 +0200

    Create Map.kt

---
+++ src/main/kotlin/de/dinomarlir/ffa/map/Map.kt
@@ -0,0 +1,14 @@
+package de.dinomarlir.ffa.map
+
+import kotlinx.serialization.Serializable
+import javax.xml.stream.Location
+
+@Serializable
+data class Map(
+    val name: String,
+    val description: String,
+    val builder: List<String>,
+    val spawnLocation: Location,
+    val borderPos1: Location,
+    val borderPos2: Location
+)
